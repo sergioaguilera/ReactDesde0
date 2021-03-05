0a1ec8b0c4bd7c24c59a8c4083c2fa6e86bc24ff
Ingenieriadigitalmx <80076850+Ingenieriadigitalmx@users.noreply.github.com>
2021-03-05 17:35:08 -0600

    contador realizado, adicionalmente se usa operador ternario

--- proyecto1/proyecto1/src/components/Contador.jsx
+++ proyecto1/proyecto1/src/components/Contador.jsx
@@ -1,12 +1,25 @@
-import Reac,{useState} from 'react'
+import React from 'react'
 
 const Contador = () => {
-    return (
-        <div>
-            <hr></hr>
-            <h2>Contador</h2>
-        </div>
-    )
+
+    const [contador,setContador] = React.useState(0)
+    
+    const sumaCont =() =>{
+
+        console.log('click en el contador')
+        setContador(contador+1)
+
+    }
+
+        return (
+            <div>
+                <hr></hr>
+                <h2>Contador</h2>
+                <h3>{contador}</h3>
+                <h4>{contador === 5 ?'por el culo te la inco' : 'aaaa que chiido' }</h4>
+                <button onClick={() => sumaCont()}>+1 al contador</button>
+            </div>
+        )
 }
 
 export default Contador
