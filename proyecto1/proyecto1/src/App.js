477f65bcdad9f05819090046ab87707b9ca77883
Ingenieriadigitalmx <80076850+Ingenieriadigitalmx@users.noreply.github.com>
2021-03-05 18:08:46 -0600

    Listas  con ReactJS

--- proyecto1/proyecto1/src/App.js
+++ proyecto1/proyecto1/src/App.js
@@ -1,16 +1,18 @@
-import Parrafo from './components/Parrafo';
+/* import Parrafo from './components/Parrafo';
 import Variables from './components/Variables';
 import Eventos from './components/Eventos';
-import Contador from './components/Contador';
+import Contador from './components/Contador'; */
+import Listas from './components/Listas';
 
 function App() {
   return (
     <div>
       <h1>Hola mundo React</h1>
-      <Parrafo />
+  {/*     <Parrafo />
       <Variables />
       <Eventos/>
-      <Contador/>
+      <Contador/> */}
+      <Listas/>
     </div>
   );
 }
